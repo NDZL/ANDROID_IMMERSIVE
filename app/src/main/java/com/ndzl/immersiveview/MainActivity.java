e0800c3db14af2140f7ea96a09945b7697073c89
CXNT48 <CXNT48@gmail.com>
2019-03-25 17:55:59 +0100

    IMEMRSIVE NON STICKY

--- app/src/main/java/com/ndzl/immersiveview/MainActivity.java
+++ app/src/main/java/com/ndzl/immersiveview/MainActivity.java
@@ -1,10 +1,19 @@
 package com.ndzl.immersiveview;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Path;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.QuickContactBadge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 /*
@@ -15,11 +24,14 @@ public class MainActivity extends Activity {
 
     static boolean isNavbarVisible = true;
 
+
+
     Button btHideShowNavBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         btHideShowNavBar = findViewById(R.id.btHideShowNavBar);
         btHideShowNavBar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,12 +39,11 @@ public class MainActivity extends Activity {
                 if(isNavbarVisible){
                     isNavbarVisible=false;
                     View decorView = getWindow().getDecorView();
-                    //int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
                     int uiOptions =  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
                     decorView.setSystemUiVisibility(uiOptions);
@@ -46,5 +57,63 @@ public class MainActivity extends Activity {
                                                 }
                                             }
         );
+*/
+        //updateUI();
+
+
+        if(isNavbarVisible){
+            isNavbarVisible=false;
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE
+
+            );
+        }
+        else {
+            isNavbarVisible=true;
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
+
+
+
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        updateUI();
+    }
+
+
+
+
+
+    public void updateUI() {
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE
+                                    );
+                }
+            }
+        });
+    }
+
+
 }
